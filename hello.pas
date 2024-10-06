798bf18551349d042778abba5fcde946e951af22
chirkov2005 <artcherk2005@gmail.com>
2024-10-06 21:48:44 +0300

    Added multi-language greeting

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Привет, мир!'); 
+  var lang := ReadString('Enter the lanuauge: ');
+  
+  case lang of
+    'русский', 'russian': Print('Здравствуйте!');
+    'english': Print('Hello!');
+    'deutsch', 'germany': Print('Hallo!');
+  end;
 end.
